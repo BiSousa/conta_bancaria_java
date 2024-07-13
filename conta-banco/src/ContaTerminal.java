7fc3a3cb0ceeb4ad0f397c5f5c9aacf7a711b26c
Beatriz Sousa <biacsousa72@gmail.com>
2024-07-13 13:19:59 -0300

    program

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("----- Bem-vindo a conta bancária -----");
+        System.out.print("Conta: ");
+        String conta = scanner.next();
+
+        System.out.print("Agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Cliente: ");
+        String cliente = scanner.next();
+
+        System.out.print("Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + 
+        agencia + " conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
